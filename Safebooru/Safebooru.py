07384c5199ff0324ebacd5606d03e2f0378a15e3
RandomDentyne <randomdentyne@gmail.com>
2017-02-12 03:19:16 -0800

    actually added the waifulist functionality

--- Safebooru/Safebooru.py
+++ Safebooru/Safebooru.py
@@ -5,7 +5,7 @@ import random
 import json
 import os 
 
-from cogs.util.dataIO import dataIO
+from cogs.utils.dataIO import dataIO
 from requests.auth import HTTPBasicAuth
 
 class Safebooru:
@@ -16,10 +16,10 @@ class Safebooru:
         self.lastWaifuRolled = {}
         invalidLists = []
         for userId in os.listdir("data/WaifuList"):
-            if not dataIO.is_valid_json(userId):
+            if not dataIO.is_valid_json("data/WaifuList/" + userId):
                 invalidLists.append(userId + "\n")
             else:
-                waifuLists[int(userId[:-5])] = dataIO.load_json(userId)
+                self.waifuLists[userId[:-5]] = dataIO.load_json("data/WaifuList/" + userId)
         if not len(invalidLists) == 0:
             print("Warning: the following files were not saved properly, and have been lost: \n")
             for user in invalidLists:
@@ -48,34 +48,36 @@ class Safebooru:
         return 
 
     @commands.command(pass_context=True)
-    async def marryWaifu(self, ctx):
+    async def marry_waifu(self, ctx):
         author = ctx.message.author
-        waifu = lastWaifuRolled.get(author.id)
+        waifu = self.lastWaifuRolled.get(author.id)
         authorFile = "data/WaifuList/" + str(author.id) + ".json"
         if waifu == None:
             await self.bot.say("No character rolled this session.")
             return
-        waifuList = waifuLists.get(author.id)
+        waifuList = self.waifuLists.get(author.id)
         if waifuList == None:
-            waifuLists[author.id] = {"name": author.name, "waifu_list": [waifu]}
-            dataIO.save_json(authorFile, waifuLists[author.id])
+            self.waifuLists[author.id] = {"name": author.name, "waifu_list": [waifu]}
+            dataIO.save_json(authorFile, self.waifuLists[author.id])
+            self.lastWaifuRolled[author.id] = None
             await self.bot.say("Waifu successfully married!")
             return
         if len(waifuList["waifu_list"]) >= 5:
             await self.bot.say("Max number of waifus reached! Please divorce a waifu before marrying more.")
             return
-        waifuLists[author.id]["waifu_list"].append(waifu)
-        dataIO.save_json(authorFile, waifuLists[author.id])
+        self.waifuLists[author.id]["waifu_list"].append(waifu)
+        dataIO.save_json(authorFile, self.waifuLists[author.id])
+        self.lastWaifuRolled[author.id] = None
         await self.bot.say("Waifu successfully married!")
         return
 
     @commands.command(pass_context=True)
-    async def listWaifus(self, ctx):
+    async def waifu_list(self, ctx):
         author = ctx.message.author
-        lastRolled = lastWaifuRolled.get(author.id)
+        lastRolled = self.lastWaifuRolled.get(author.id)
         if lastRolled != None:
             await self.bot.say("Last waifu rolled: " + lastRolled["name"] + "\n" + lastRolled["img"] + "\n")
-        waifuList = waifuLists.get(author.id)
+        waifuList = self.waifuLists.get(author.id)
         if waifuList == None or len(waifuList["waifu_list"]) == 0:
             await self.bot.say("No waifus married yet! Go marry some waifus!")
             return
@@ -83,17 +85,18 @@ class Safebooru:
         i = 0
         for waifu in waifuList["waifu_list"]:
             await self.bot.say("[" + str(i) + "] " + waifu["name"] + "\n" + waifu["img"] + "\n")
+            i = i + 1
         return
 
     @commands.command(pass_context=True)
-    async def divorceWaifu(self, ctx, index: int):
+    async def divorce_waifu(self, ctx, index: int):
         author = ctx.message.author
-        waifuList = waifuLists.get(author.id)
+        waifuList = self.waifuLists.get(author.id)
         if waifuList == None or len(waifuList["waifu_list"]) < index:
             await self.bot.say("Invalid index")
             return
-        waifuLists[author.id]["waifu_list"].delete(index)
-        dataIO.save_json("data/WaifuList/" + str(author.id) + ".json", waifuLists[author.id])
+        self.waifuLists[author.id]["waifu_list"].pop(index)
+        dataIO.save_json("data/WaifuList/" + str(author.id) + ".json", self.waifuLists[author.id])
         await self.bot.say("Waifu successfully divorced.")
         return
         
@@ -108,7 +111,7 @@ class Safebooru:
         fileUrl = reqJson.get("large_file_url")
         if fileUrl == None:
             fileUrl = reqJson.get("file_url")
-        lastWaifuRolled[user.id] = {"name": waifuName, "img": fileUrl}
+        self.lastWaifuRolled[user.id] = {"name": waifuName, "img": "https://safebooru.donmai.us" + fileUrl}
         return waifuName + "\nhttps://safebooru.donmai.us" + fileUrl
 
 def checkFolders():
