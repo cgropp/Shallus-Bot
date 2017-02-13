cfa6956e9a425fd07a804871d8f01e715deedb0f
Coleena <papercoleena@gmail.com>
2017-02-13 13:10:13 -0800

    Added stats tracking for safebooru commands

--- Safebooru/Safebooru.py
+++ Safebooru/Safebooru.py
@@ -32,6 +32,8 @@ class Safebooru:
         params = {"tags": u'1girl solo' }
         linkName = await self.getSafebooruLink(params, ctx.message.author)
         await self.bot.say("Here is your waifu: " + linkName)
+
+        await StatsTracker.updateStat(self, ctx.message.author.id, ctx.message.content[1:])
         return 
         
     @commands.command(pass_context=True)
@@ -39,6 +41,8 @@ class Safebooru:
         params = {"tags": u'holding_hands yuri' }
         linkName = await self.getSafebooruLink(params, ctx.message.author)
         await self.bot.say("Here's some (SFW) yuri: " + linkName)
+
+        await StatsTracker.updateStat(self, ctx.message.author.id, ctx.message.content[1:])
         return 
         
     @commands.command(pass_context=True)
@@ -46,6 +50,8 @@ class Safebooru:
         params = {"tags": u'1boy solo' }
         linkName = await self.getSafebooruLink(params, ctx.message.author)
         await self.bot.say("Here's your husbando: " + linkName)
+
+        await StatsTracker.updateStat(self, ctx.message.author.id, ctx.message.content[1:])
         return 
 
     @commands.command(pass_context=True)
@@ -102,7 +108,7 @@ class Safebooru:
         lastDelete = waifuList.get("last_delete")
         if lastDelete != None and time.time() - float(lastDelete) < (5 * 24 * 60 * 60):
             await self.bot.say("It hasn't been 5 days since your last divorce! Spare some hearts, would ya?")
-            return
+         #   return
         self.waifuLists[author.id]["waifu_list"].pop(index)
         self.waifuLists[author.id]["last_delete"] = time.time()
         dataIO.save_json("data/WaifuList/" + str(author.id) + ".json", self.waifuLists[author.id])
@@ -129,8 +135,47 @@ def checkFolders():
     if not os.path.exists("data/WaifuList"):
         print("Creating directory data/WaifuList...")
         os.makedirs("data/WaifuList")
+    if not os.path.exists("data/stats"):
+        print("Creating directory data/stats...")
+        os.makedirs("data/stats")
 
-    
+
+class StatsTracker:
+    async def updateStat(self, userid, commandname):
+        datapath = "data/stats"
+
+        # Create directory if does not exist
+        if not os.path.exists(datapath):
+            print("Creating stats data directory...")
+            os.makedirs(datapath)
+
+        # Create JSON file if does not exist or if invalid
+        invalidJSON = False
+        if not os.path.isfile(datapath + "/" + userid + ".json"):
+            await self.bot.say("No userdata exists for " + userid + "! Creating...")
+            invalidJSON = True
+        elif not dataIO.is_valid_json(datapath + "/" + userid + ".json"):
+            await self.bot.say("Invalid stats JSON found. All your stats are gone forever. Blame a dev :^(")
+            invalidJSON = True
+
+        if(invalidJSON):
+            data = {}
+            dataIO.save_json(datapath + "/" + userid + ".json", data)
+
+
+        # Read in JSON file, increment command count, and write
+        userdata = dataIO.load_json(datapath + "/" + userid + ".json")
+        if commandname not in userdata:
+            userdata[commandname] = 0
+
+        userdata[commandname] += 1
+        dataIO.save_json(datapath + "/" + userid + ".json", userdata)
+
+        return
+
+    @commands.command(pass_context=True)
+    async def stats(self, ctx):
+        return
 
 def setup(bot):
     checkFolders()
---
+++ Stats/Stats.py
@@ -0,0 +1,45 @@
+import requests
+import discord
+from discord.ext import commands
+import json
+import os
+
+from cogs.utils.dataIO import dataIO
+
+class Stats:
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(pass_context=True)
+    async def stats(self, ctx):
+        datapath = "data/stats"
+        userid = ctx.message.author.id
+        displayname = ctx.message.author.display_name
+
+        # Can't print if data invalid or if
+        if not os.path.isfile(datapath + "/" + userid + ".json"):
+            await self.bot.say("You don't have any stats tracked, " + displayname + "! Try using some commands.")
+            return
+        elif not dataIO.is_valid_json(datapath + "/" + userid + ".json"):
+            await self.bot.say("Invalid stats JSON found. All your stats are gone forever. Blame a dev :^(")
+            data = {}
+            dataIO.save_json(datapath + "/" + userid + ".json", data)
+            return
+
+        output = ""
+        # Print all available stats
+        userdata = dataIO.load_json(datapath + "/" + userid + ".json")
+        for commandname, count in userdata.items():
+            output += str.capitalize(commandname) + ":  " + str(count) + "\n"
+        await self.bot.say(output)
+
+        return
+
+def checkFolders():
+    if not os.path.exists("data/stats"):
+        print("Creating directory data/stats...")
+        os.makedirs("data/stats")
+
+def setup(bot):
+    checkFolders()
+    bot.add_cog(Stats(bot))
