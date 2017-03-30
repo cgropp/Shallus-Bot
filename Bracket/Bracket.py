b3b83de62c81749c77e86b826360f454ab5e777d
ryanyi <ryayi387@gmail.com>
2017-03-29 21:11:54 -0700

    Allows Bot to begin tracking a tournament. Get player's opponent, the match's players, and also update scores

---
+++ Bracket/Bracket.py
@@ -0,0 +1,382 @@
+import discord 
+from discord.ext import commands
+from cogs.utils.dataIO import dataIO
+import challonge
+import json
+import os
+
+#This class will allow users in a server to track challonge
+#tournaments. Looking up opponents and updating scores will
+#also be supported.
+class Bracket:
+    """Begin/Stop tracking a tournament, update scores, get next opponent, get players of a match"""
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.username = None
+        self.api = None
+    
+    @commands.command(pass_context=True)
+    async def track(self, ctx, url):
+        """Begin tracking a Challonge tournament
+        Don't include the challonge.com part."""
+        serverId = ctx.message.server.id
+        path = "data/bracket/" + serverId + "/tracklist.json"
+        author = ctx.message.author
+        
+        #make sure the server has a folder for tracklist to be inside.
+        if not os.path.exists("data/bracket/" + serverId):
+            print("Creating a new directory for server " + ctx.message.server.name)
+            os.makedirs("data/bracket/" + serverId)
+        
+        #check to see if a tournament is being tracked or not.
+        if not dataIO.is_valid_json(path):
+            print("Creating new trackfile for server with id " + serverId)
+            dataIO.save_json(path, {})
+        else:
+            await self.bot.say("A tournament is already being tracked. Please stop tracking your current tournament to track a new tournament.")
+            return
+
+        #establish connection to challonge and get tournament data
+        userdata = dataIO.load_json("data/bracket/login.json")
+        challonge.set_credentials(userdata["username"], userdata["api_key"])
+        tourn = challonge.tournaments.show(url)
+        await self.bot.say("Found a tournament with this url called " + tourn["name"] + ". Begin tracking this? (yes/no)")
+
+        answer = await self.bot.wait_for_message(timeout=15, author=author)
+            
+        if answer == None or not answer.content.lower().strip() == "yes": 
+            os.remove(path)
+            await self.bot.say("No action was taken. I did not track " + tourn["name"])
+            return
+        
+        #JSON object of matches
+        jsonMatch = []
+        item = {}
+        matches = challonge.matches.index(tourn["id"])
+        for entry in matches:
+                item[entry["identifier"]] = entry["id"]
+        jsonMatch.append(item)
+
+        #JSON object of players
+        jsonPlayer = []
+        item = {}
+        players = challonge.participants.index(tourn["id"])
+        for player in players:
+            item[player["name"]] = player["id"]
+        jsonPlayer.append(item)
+        
+        #the JSON data will contain id, url, name, and JSON objects of the matches and players.
+        info = info = {"id": tourn["id"], "url": url, "name": tourn["name"], "matches": jsonMatch, "players": jsonPlayer}
+        dataIO.save_json(path, info)
+        await self.bot.say("Tracking tournament " + tourn["name"] + ".")
+
+    @commands.command(pass_context=True)
+    async def stoptrack(self, ctx):
+        """Stop tracking a tournament"""
+
+        path = "data/bracket/" + ctx.message.server.id + "/tracklist.json"
+        author = ctx.message.author
+
+        #check to see if a tournament is being tracked or not. Remove tracklist file to stop tracking.
+        if dataIO.is_valid_json(path):
+            userdata = dataIO.load_json(path)
+            name = userdata["name"]
+            await self.bot.say("Currently tracking " + name + ". Are you sure you want to stop tracking this tournament? (yes/no)")
+            answer = await self.bot.wait_for_message(timeout=15, author=author)
+            
+            if answer == None or not answer.content.lower().strip() == "yes": 
+                await self.bot.say("No action was taken. I did not stop tracking " + name)
+            else:
+                os.remove(path)
+                await self.bot.say("Not longer tracking " + name + ".")
+        
+        #No track file found.
+        else:
+            await self.bot.say("Currently not tracking a tournament.")
+
+    @commands.command(pass_context=True)
+    async def opponent(self, ctx, player):
+        """Give the opponent of the player.\nCASE SENSITIVE!"""
+        
+        path = "data/bracket/" + ctx.message.server.id + "/tracklist.json"
+
+        #check to see if a tournament is being tracked or not. Search through matches if a tournament is being tracked.
+        if dataIO.is_valid_json(path):
+            userdata = dataIO.load_json(path)
+            
+            #Make sure player exists by checking the players list in the JSON file.
+            if not player in userdata["players"][0]:
+                await self.bot.say("This player name is not in the tournament. Please double check spelling and capitalization.")
+            else:
+                await self.bot.say("Searching...")
+                
+                #obtain the login credentials and login.
+                login = dataIO.load_json("data/bracket/login.json")
+                challonge.set_credentials(login["username"], login["api_key"])
+
+                playerId = userdata["players"][0][player]
+                matches = challonge.matches.index(userdata["id"])
+
+                for match in matches:
+                    
+                    #Given player is either not in this match, or the winner of the match has already been decided.
+                    #Ignore these cases.
+                    if (not match["winner_id"] == None) or (not playerId == match["player1_id"] and not playerId == match["player2_id"]):
+                            continue
+                    else:
+                        
+                        #Player is player 1. Return information about player 2 in this scenario.
+                        if playerId == match["player1_id"]:
+                            
+                            #There is a player 2. Just return the name of player 2.
+                            if not match["player2_id"] == None:
+                                await self.bot.say(player + "'s opponent will be " + challonge.participants.show(userdata["id"], match["player2_id"])["name"])
+                                break
+                            else:
+                                
+                                #No player 2. This means that the match to determine player 2 hasn't been completed. Print conditions of previous match to determine player 2.
+                                if match["player2_is_prereq_match_loser"] == False:
+                                    await self.bot.say(player + "'s opponent will be the winner of Match " + challonge.matches.show(userdata["id"], match["player2_prereq_match_id"])["identifier"])
+                                    break
+                                else:
+                                    await self.bot.say(player + "'s opponent will be the loser of Match " + challonge.matches.show(userdata["id"], match["player2_prereq_match_id"])["identifier"])
+                                    break
+                        
+                        #Player is player 2. Return information about player 1 in this scenario.
+                        else:
+                            
+                            #There is a player 1. Just return name of player 1.
+                            if not match["player1_id"] == None:
+                                await self.bot.say(player + "'s opponent will be " + challonge.participants.show(userdata["id"], match["player1_id"])["name"])
+                                break
+                            else:
+                                
+                                #No player 1. This means that the match to determine player 2 hasn't been completed. Print conditions of previous match to determine player 2.
+                                if match["player1_is_prereq_match_loser"] == False:
+                                    await self.bot.say(player + "'s opponent will be the winner of Match " + challonge.matches.show(userdata["id"], match["player1_prereq_match_id"])["identifier"])
+                                    break
+                                else:
+                                    await self.bot.say(player + "'s opponent will be the loser of Match " + challonge.matches.show(userdata["id"], match["player1_prereq_match_id"])["identifier"])
+                                    break
+
+        #No track file found.
+        else:
+            await self.bot.say("Currently not tracking a tournament.")
+
+    @commands.command(pass_context=True)
+    async def updatescore(self, ctx, player1, player2, score1, score2):
+        """Update the scores of a match"""
+
+        if player1 == player2:
+            await self.bot.say("You entered the same name twice. Please enter different names.")
+            return
+      
+        path = "data/bracket/" + ctx.message.server.id + "/tracklist.json"
+        author = ctx.message.author
+
+        #check to see if a tournament is being tracked or not. Change match score if a tournament is being tracked.
+        if dataIO.is_valid_json(path):
+
+            userdata = dataIO.load_json(path)
+
+
+            #make sure player exists by checking the players list in the JSON file
+            if not player1 in userdata["players"][0] and not player2 in userdata["players"][0]:
+                await self.bot.say("At least one of the 2 players is not in the tournament. Please double check spelling and capitalization.")
+
+            else:
+
+                #attempt to convert the scores entered to integers.
+                try:
+                    set1 = int(score1)
+                    set2 = int(score2)
+
+                    if set1 == set2:
+                        await self.bot.say("You entered the same score. Only update the score when the match has been concluded.")
+                        return
+
+                except ValueError:
+                    await self.bot.say("Please enter integers for the scores")
+                    return
+
+                #obtain the login credentials and login.
+                login = dataIO.load_json("data/bracket/login.json")
+                challonge.set_credentials(login["username"], login["api_key"])
+
+                matches = challonge.matches.index(userdata["id"])
+
+                #get ids for the players
+                player1id = userdata["players"][0][player1]
+                player2id = userdata["players"][0][player2]
+
+                #search through all of the matches until we find the match with the 2 players, and update the score.
+                for match in matches:
+                    
+                    #The 2 players are not playing each other in this match. Skip to next match.
+                    if not (match["player1_id"] == player1id and match["player2_id"] == player2id) and not (match["player1_id"] == player2id and match["player2_id"] == player1id):
+                        continue
+                    else:
+                        
+                        #rearrange names/scores with temp vars to help with updating score.
+                        actualPlayer1 = ""
+                        actualPlayer2 = ""
+                        actual1id = ""
+                        actual2id = ""
+                        actualScore1 = 0
+                        actualScore2 = 0
+
+                        #match player1 is the same as player1
+                        if match["player1_id"] == player1id:
+                            actualPlayer1 = player1
+                            actualPlayer2 = player2
+                            actual1id = player1id
+                            actual2id = player2id
+                            actualScore1 = set1
+                            actualScore2 = set2
+
+                        #match player1 is the same as player2
+                        else:
+                            actualPlayer1 = player2
+                            actualPlayer2 = player1
+                            actual1id = player2id
+                            actual2id = player1id
+                            actualScore1 = set2
+                            actualScore2 = set1
+
+                        #Also need to determine winner and their id
+                        winner = ""
+                        winnerid = ""
+                        loserid = ""
+
+                        #Score1 > Score2 means player 1 is the winner. Else player 2 is.
+                        if actualScore1 > actualScore2:
+                            winner = actualPlayer1
+                            winnerid = actual1id
+                            loserid = actual2id
+                        else:
+                            winner = actualPlayer2
+                            winnerid = actual2id
+                            loserid = actual1id
+
+                        await self.bot.say(actualPlayer1 + " vs " + actualPlayer2 + "\nThe score was " + str(actualScore1) + "_" + str(actualScore2) + ".\nThe winner is " + winner + ".\nMake sure this is correct.\nProceed with update? (yes/no)")
+                        
+                        answer = await self.bot.wait_for_message(timeout=15, author=author)
+                        if answer == None or not answer.content.lower().strip() == "yes": 
+                            await self.bot.say("Score was not updated.")
+                        else:
+                            score_csv = str(actualScore1) + "-" + str(actualScore2)
+                            print(match)
+                            newmatch = {"match": match}
+                            
+                            #newmatch["loser_id"] = loserid
+                            #newmatch["winner_id"] = winnerid
+                            #newmatch["scores_csv"] = score_csv
+                            #newmatch["player1_votes"] = actualScore1
+                            #newmatch["player2_votes"] = actualScore2
+                            #newmatch["state"] = "complete"
+
+                            #results = {"match": newmatch}
+
+
+                            challonge.matches.update(userdata["id"], match["id"], scores_csv = score_csv, winner_id = winnerid, player1_votes = actualScore1, player2_votes = actualScore2)
+                            await self.bot.say("Score was updated.")
+                        return
+
+
+
+                #If we exit the loop, this means that the 2 players are not playing in the same match.
+                await self.bot.say("Found no match where these 2 players are fighting. Please double check the player tags.")
+                return
+
+
+        #No track file found.
+        else:
+            await self.bot.say("Currently not tracking a tournament.")
+
+    @commands.command(pass_context=True)
+    async def match(self, ctx, letter):
+        """List the players of a given match\nUse only capital letters."""
+        
+        path = "data/bracket/" + ctx.message.server.id + "/tracklist.json"
+
+        #check to see if a tournament is being tracked or not. Search through matches if a tournament is being tracked.
+        if dataIO.is_valid_json(path):
+            userdata = dataIO.load_json(path)
+
+            #Make sure match exists by checking the matches list in the JSON file.
+            if not letter in userdata["matches"][0]:
+                await self.bot.say("There is no match corresponding to this letter.")
+            else:
+
+                await self.bot.say("Searching...")
+
+                #obtain the login credentials and login.
+                login = dataIO.load_json("data/bracket/login.json")
+                challonge.set_credentials(login["username"], login["api_key"])
+
+                match = challonge.matches.show(userdata["id"], userdata["matches"][0][letter])
+                print(match)
+
+                #Both players are decided in the match.
+                if not match["player1_id"] == None and not match["player2_id"] == None:
+                    await self.bot.say(challonge.participants.show(userdata["id"], match["player1_id"])["name"] + " will be facing " + challonge.participants.show(userdata["id"], match["player2_id"])["name"] + ".")
+
+                    #If winner has been determined, then also say who won the match.
+                    if not match["winner_id"] == None:
+                        await self.bot.say("This match has concluded. The winner was " + challonge.participants.show(userdata["id"], match["winner_id"])["name"])
+
+                else:
+                    player1 = ""
+                    player2 = ""
+                    
+                    #Player 1 hasn't been determined yet.
+                    if match["player1_id"] == None:
+                        if match["player1_is_prereq_match_loser"] == False:
+                            player1 = "The winner of Match " + challonge.matches.show(userdata["id"], match["player1_prereq_match_id"])["identifier"]
+                        else:
+                            player1 = "The loser of Match " + challonge.matches.show(userdata["id"], match["player1_prereq_match_id"])["identifier"]
+
+                    #Player 1 is determined.
+                    else:
+                        player1 = challonge.participants.show(userdata["id"], match["player1_id"])["name"]
+
+                    #Player 2 hasn't been determined yet.
+                    if match["player2_id"] == None:
+                        if match["player2_is_prereq_match_loser"] == False:
+                            player2 = "the winner of Match " + challonge.matches.show(userdata["id"], match["player2_prereq_match_id"])["identifier"]
+                        else:
+                            player2 = "the loser of Match " + challonge.matches.show(userdata["id"], match["player2_prereq_match_id"])["identifier"]
+
+                    #Player 2 is determined.
+                    else:
+                        player2 = challonge.participants.show(userdata["id"], match["player2_id"])["name"]
+
+                    await self.bot.say(player1 + " will be facing " + player2 + ".")
+
+
+        #No track file found.
+        else:
+            await self.bot.say("Currently not tracking a tournament.")
+
+
+        
+
+#make sure all of the correct files are created already.
+def check_files():
+    f = "data/bracket/login.json"
+    if not dataIO.is_valid_json(f):
+        print("Creating a new login file.")
+        login = {"username": "Nemia", "api_key": "uS247Tv0LWZn0tK2MTaXkux1x7dWLq3SXxbL4ieX"}
+        dataIO.save_json(f, login)
+
+#make sure all of the required folders are created already.
+def check_folders():
+    if not os.path.exists("data/bracket"):
+        print("Creating data/bracket folder...")
+        os.makedirs("data/bracket")
+
+def setup(bot):
+    check_folders()
+    check_files()
+    bot.add_cog(Bracket(bot))
