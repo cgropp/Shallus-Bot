132a66673c84ea91d81b4ed5e847dccbbd6b36c3
Cameron Gropp <cgropp@ucsd.edu>
2017-07-07 22:18:04 -0700

    Limited votes to 1 per user, implemented framework for time based polls but need to figure out solution that doesn't freeze entire bot

--- WaifuWars/WaifuWars.py
+++ WaifuWars/WaifuWars.py
@@ -2,6 +2,7 @@ import discord
 import time
 import requests
 import configparser
+import time
 
 from requests.auth import HTTPBasicAuth
 from discord.ext import commands
@@ -13,6 +14,8 @@ class WaifuWars:
         self.bot = bot
         self.waifu1votes = 0
         self.waifu2votes = 0
+        #Initialize empty set for users that have already voted
+        self.alreadyVoted = set({})
 
         parser = configparser.ConfigParser()
         parser.read('data/auth/auth.ini')
@@ -29,17 +32,63 @@ class WaifuWars:
     @commands.command(pass_context=True)
     async def waifuWar(self, ctx):
         """Posts a random waifu from Safebooru."""
+        #Clear the set of already voted users, as a new vote has started
+        self.alreadyVoted.clear()
+        self.waifu1votes = 0
+        self.waifu2votes = 0
+        
+        #Receive waifu
         params = {"tags": u'1girl solo'}
         linkName = await self.getSafebooruLink(params, ctx.message.author)
         linkName2 = await self.getSafebooruLink(params, ctx.message.author)
-        await self.bot.say("Debug statement, unfinished command: Vote for the superior waifu with !waifuvote 1 or !waifuvote 2 ")
-        await self.bot.say("Waifu #1: " + linkName + "\nWaifu #2: " + linkName2)
+        
+        #Print out waifus
+        await self.bot.say("Debug statement, unfinished command: Vote for the superior waifu with !waifuvote 1 or !waifuvote 2")
+        await self.bot.say("Waifu #1: " + linkName)
+        # Making it two seperate print statements to ensure the previews display in correct order
+        await self.bot.say("Waifu #2: " + linkName2)
+        
+        
+        #TODO: Fix later to not stall entire bot
+        
+        # Print results after time runs out
+        # Don't use sleep, freezes entire bot
+        
+        # Calculate winner
+        # await self.bot.say("Debug statement: timer currently set to 30 seconds, will make longer later")
+        
+        
+        # if (self.waifu1votes == self.waifu2votes):
+            # await self.bot.say("The waifu war resulted in a tie with " +self.waifu2votes + " votes for each waifu!")
+            # return
+
+        # elif (self.waifu1votes >self.waifu2votes):
+            # winner = linkName
+            # winnerVotes = self.waifu2votes
+            # loserVotes = self.waifu2votes
+        # else:
+            # winner = linkName2
+            # winnerVotes =self.waifu2votes
+            # loserVotes = self.waifu2votes
+
+        # await self.bot.say("The waifu war is over. " + winner + "has triumphed over her opponent with " + winnerVotes + " votes vs " + loserVotes + " votes.")
      
         return
 
     @commands.command(pass_context=True)
     async def waifuvote(self, ctx, voteNum: int):
         """Keeps track of votes."""
+        
+        #Checks if they have already voted
+        userid = ctx.message.author.id
+        if (userid in self.alreadyVoted):
+            await self.bot.say("You've already voted for a waifu, " + ctx.message.author.mention + "! Please wait until the next vote starts before voting again.")
+            return
+        #If they haven't voted, add their id to the set
+        else:
+            self.alreadyVoted.add(userid)
+        
+        #Allows user to vote for waifu #1 or waifu #2
         if (voteNum == 1):
             self.waifu1votes += 1
             await self.bot.say("You have voted for waifu #" + str(voteNum) + ". This waifu now has " + str(self.waifu1votes) + " vote(s)." )
